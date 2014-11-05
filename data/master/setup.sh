307755887775fc6d95338dd4191df2f64f82d8ef
Doug Scheirer <dscheirer@perforce.com>
2014-11-05 15:49:56 -0800

    adding dnsmasq and initial master config for replica

--- data/master/setup.sh
+++ data/master/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# setup.sh server-id
 
 function die()
 {
         echo $1 && exit 1
 }
 
+SERVERID=$1
 P4D_DIR="/var/perforce/p4d"
 
 # clean the data dir, start fresh
@@ -20,8 +22,16 @@ mkdir -p $P4D_DIR || die "failed to create p4d directory"
 export PATH=$PATH:/opt/perforce/bin/
 export P4PORT="localhost:1666"
 p4 info || die "failed to get p4 info"
+p4 serverid $SERVERID 
 
 # set up users, etc.
 p4 -u super user -o | p4 -u super user -i
 p4 -u super protect -o | p4 -u super protect -i
 ( p4 -u super user -o service ; echo -e "\nType: service\n" ) | p4 -u super user -i -f
+
+# dnsmasq config
+echo "address=/master/192.168.33.10" >> /etc/dnsmasq.conf && service dnsmasq restart || die "failed to configure master dnsmasq"
+echo "address=/replica1/192.168.33.11" >> /etc/dnsmasq.conf && service dnsmasq restart || die "failed to configure replica1 dnsmasq"
+
+# make Replica1 a RO replica of Master 
+p4 -p master:11111 configure set Replica1#P4TARGET=master:11111
